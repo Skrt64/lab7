f1b9df7222492d947e3de6757883d89c26b9b50b
skrt <farm.shk@gmail.com>
2021-11-30 15:13:25 +0700

    Lab7_1_complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,17 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+    string text;
+	cout << "Input text: ";
+	cin >> text;
+    cout << "Reversed text: ";
+    cout << func1(text) <<"\n";
+    cout << "Palindrome: ";
+	
+	if(func2(func1(text)) == func2(text)){
+		cout << "Yes";
+	}else{
+		cout << "No";
+	}
     return 0;
 }
